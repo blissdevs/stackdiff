dc06e849e67f5127de7ec556b9e2654bbfb8429c
Blake Issacs <blissdevs@users.noreply.github.com>
2026-04-20 01:14:27 +0800

    docs: add module-level JSDoc comment to splitter index
    fixes #7

--- src/splitter/index.ts
+++ src/splitter/index.ts
@@ -1,2 +1,6 @@
+/**
+ * Splitter module for dividing environment variable maps into logical groups.
+ * Provides utilities for splitting, filtering, and reporting on env var collections.
+ */
 export { splitEnvMap, splitByPredicate, splitByPrefix, formatSplitReport } from "./envSplitter";
 export type { SplitResult } from "./envSplitter";
