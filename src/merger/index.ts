4863bc19dcc3b3d82e570b2ccfbd19de837d3208
Blake Issacs <blissdevs@users.noreply.github.com>
2026-04-30 03:35:19 +0800

    docs: add module-level JSDoc comment to merger index

--- src/merger/index.ts
+++ src/merger/index.ts
@@ -1,2 +1,6 @@
+/**
+ * @module merger
+ * Provides utilities for merging environment variable maps and handling merge conflicts.
+ */
 export { mergeEnvMaps, formatMergeConflicts } from './envMerger';
 export type { MergeOptions, MergeStrategy, MergeConflict, MergeResult } from './envMerger';
